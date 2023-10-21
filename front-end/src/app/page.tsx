74954e7c0d903e371a2ce14e94d2fee658c9bee6
thien.duongv <thien.duongv@one-line.com>
2023-10-22 06:43:09 +0700

    test: 1

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-6xl font-bold text-center">Hello World</h1>
+        <h1 className="text-6xl font-bold text-center">Hello World 111</h1>
     </main>
   )
 }
